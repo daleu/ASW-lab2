8225ebec29e3d0b82d95d4c2dce196888557497b
Eduard Maura I Puig <eduard.maura.i@a5s113pc23.fib.upc.es>
2016-09-27 18:49:54 +0200

    tasca #3 finalitzada

--- client.php
+++ client.php
@@ -1,8 +1,11 @@
 <?php
 
 $URI = 'http://localhost:8080/waslab02/wall.php';
-$resp = file_get_contents($URI);
+$alltweets = new SimpleXMLElement(file_get_contents($URI));
 echo $http_response_header[0], "\n"; // Print the first HTTP response header
-echo $resp;  // Print HTTP response body
+//echo $resp;  // Print HTTP response body
+foreach($alltweets->tweet as $tweet) {
+  echo '[Tweet: #',$tweet["id"],'] ', $tweet->author , ': ', $tweet->text, ' [' , $tweet->time, ']', PHP_EOL;
 
+}
 ?>
