d3a20015d90ae2654c3b8dc72110e875bb67d381
Eduard Maura I Puig <eduard.maura.i@b5s101pc31.fib.upc.es>
2016-10-03 17:11:56 +0200

    tasca #6 finalitzada

--- client.php
+++ client.php
@@ -4,7 +4,7 @@ $URI = 'http://localhost:8080/waslab02/wall.php';
 $alltweets = new SimpleXMLElement(file_get_contents($URI));
 echo $http_response_header[0], "\n"; // Print the first HTTP response header
 foreach($alltweets->tweet as $tweet) {
-  echo '[Tweet: #',$tweet["id"],'] ', $tweet->author , ': ', $tweet->text, ' [' , $tweet->time, ']', PHP_EOL;
+  echo '[Tweet: #',$tweet["id"],'] ',$tweet->author,': ',$tweet->text,' [',$tweet->time,']',PHP_EOL;
 }
 
 $postdata = new SimpleXMLElement('<tweet></tweet>');
--- rss.php
+++ rss.php
@@ -1,5 +1,25 @@
 <?php
 
-echo 'Wait until Task #6 is done to get something interesting here';
+require_once("DbHandler.php");
+setlocale(LC_TIME,"en_US");
+$dbhandler = new DbHandler();
+
+$rss = new SimpleXMLElement('<rss version="2.0"></rss>');
+
+$channel = $rss->addChild('channel');
+$channel->title = "Wall of Tweets 2 - RSS Version";
+$channel->link = "http://localhost:8080/waslab02/wall.php";
+$channel->description = 'RSS 2.0 that retrieves the tweets posted to the web app "Wall of Tweets 2"';
+
+$tweets = $dbhandler->getTweets();
+
+foreach($tweets as $tweet) {
+  $item = $channel->addChild('item');
+  $item->title = $tweet['text'];
+  $item->link = "http://localhost:8080/waslab02/wall.php#item_".$tweet['id'];
+  $item->description = date("m/d/y h:i A", $tweet["time"]).'<br /> <br />This is WoT tweet #'.$tweet["id"].' posted by <b>'.$tweet["author"].'</b>. It has been liked by <b>'.$tweet["likes"].'</b> people';
+}
+
+echo $rss->asXML();
 
 ?>
